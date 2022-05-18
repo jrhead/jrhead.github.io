f820db67861bd640b87389096755af0f76e5696d
jrhead <jrhead6@gmail.com>
2022-05-18 13:29:05 -0700

    updates to assistant researcher

---
+++ gif_making.R
@@ -0,0 +1,17 @@
+## Making gif ##
+
+library(magick)
+imgs <- list.files("gif", pattern = ".jpg", full.names = TRUE)
+
+img_list <- lapply(imgs, image_read)
+
+## join the images together
+img_joined <- image_join(img_list)
+
+## animate at 2 frames per second
+img_animated <- image_animate(img_joined,
+                              fps = 0.5)
+
+## save to disk
+image_write(image = image_scale(img_animated, "2785x1855"),
+            path = "gif//lab-imgs.gif")
